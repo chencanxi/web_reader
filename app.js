9d92badb6f5172a2a494169c4e21ac2d1b8e18b2
chen-pc <1043567064@qq.com>
2017-02-25 18:19:08 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,128 @@
+var koa = require('koa');
+var controller = require('koa-route');
+var app = koa();
+
+var views = require('co-views');
+var render = views('./view',{
+	map: {html: 'ejs'}
+});
+var koa_static = require('koa-static-server');
+var ser = require('./service/webAppService.js');
+
+app.use(koa_static({
+	rootDir: './static/',
+	rootPath: '/static/',
+	maxage: 0
+}));
+
+app.use(controller.get('/route_test',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = 'hello world'; 
+}));
+
+app.use(controller.get('/ejs_test',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = yield render('test',{title:'title_test'});
+}));
+
+var querystring = require('querystring');
+
+// 页面数据
+app.use(controller.get('/',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = yield render('index',{title:'首页'});
+}));
+
+app.use(controller.get('/male',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = yield render('male',{title:'男生频道'});
+}));
+
+app.use(controller.get('/female',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = yield render('female',{title:'女生频道'});
+}));
+
+app.use(controller.get('/category',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = yield render('category',{title:'分类'});
+}));
+
+app.use(controller.get('/rank',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = yield render('rank',{title:'书架'});
+}));
+
+app.use(controller.get('/search',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = yield render('search',{title:'搜索'});
+}));
+
+app.use(controller.get('/book',function*(){
+    this.set('Cache-Control','no-cache');
+    var params = querystring.parse(this.req._parsedUrl.query);
+    var bookId = params.id;
+    this.body = yield render('book',{bookId:bookId});
+}));
+
+app.use(controller.get('/api_test',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = ser.get_test_data();
+}));
+
+app.use(controller.get('/ajax/index',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = ser.get_index_data();
+}));
+
+app.use(controller.get('/ajax/rank',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = ser.get_rank_data();
+}));
+
+app.use(controller.get('/ajax/bookbacket',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = ser.get_bookbacket_data();
+}));
+
+app.use(controller.get('/ajax/category',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = ser.get_category_data();
+}));
+
+
+
+app.use(controller.get('/ajax/num',function*(){
+    this.set('Cache-Control','no-cache');
+    var params = querystring.parse(this.req._parsedUrl.query);
+    var id = params.id;
+    if(!id){
+    	id='';
+    }
+    this.body = ser.get_num_data(id);
+}));
+
+app.use(controller.get('/ajax/female',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = ser.get_female_data();
+}));
+
+app.use(controller.get('/ajax/male',function*(){
+    this.set('Cache-Control','no-cache');
+    this.body = ser.get_male_data();
+}));
+
+
+app.use(controller.get('/ajax/search',function*(){
+    this.set('Cache-Control','no-cache');
+    
+    var params = querystring.parse(this.req._parsedUrl.query);
+    var start = params.start;
+    var end = params.end;
+    var keyword = params.keyword;
+
+    this.body = yield ser.get_search_data(start,end,keyword);
+}));
+
+app.listen(3000);
+console.log('koa is start');
